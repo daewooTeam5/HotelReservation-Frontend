1964ecbd6841ddaa0657def358256eaec8cbc4e7
devaspirant0510 <seungho020510@gmail.com>
2025-09-15 01:24:42 +0900

    feat: 사용자 타입 및 로그인 성공 DTO 추가

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,5 +1,25 @@
-export type User = {
+export type UserLegacy = {
   username: string;
   role: string;
   id: number;
 };
+
+export type Role = 'customer' | 'hotel_owner' | 'admin';
+export type Status = 'active' | 'inactive' | 'banned';
+
+export type User =  {
+  id: number;
+  userId: string;
+  email: string;
+  password?: string;
+  name: string;
+  phone?: string;
+  role: Role;
+  status: Status;
+  updatedAt: string;
+  createdAt: string;
+}
+export type LoginSuccessDto = {
+  accessToken: string;
+  user: User;
+}
