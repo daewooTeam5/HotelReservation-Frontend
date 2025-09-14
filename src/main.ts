7e4b55cb25c61473c0c11bf8a3adaa97577a08e0
devaspirant0510 <seungho020510@gmail.com>
2025-09-15 01:21:48 +0900

    feat: Toast 및 vuequery 옵션 설정

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,29 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
 import Aura from '@primeuix/themes/aura';
+import { Toast } from 'primevue';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(VueQueryPlugin);
+app.use(VueQueryPlugin, {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        retry: 0,
+      },
+    },
+  },
+});
+app.use(ToastService);
+app.component('Toast', Toast);
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
-  },
+    preset: Aura
+  }
 });
 
 app.mount('#app');
