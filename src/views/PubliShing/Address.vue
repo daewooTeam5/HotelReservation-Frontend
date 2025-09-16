471776cfde14e71af4f3dfd323709d6089557d89
user <testuser@gitlab.example.com>
2025-09-16 21:50:20 +0900

    등록 폼 기본 베이스

--- src/views/PubliShing/Address.vue
+++ src/views/PubliShing/Address.vue
@@ -1,5 +1,32 @@
-<script setup lang="ts"></script>
+<script setup>
+import { usePublishingStore } from '@/stores/publishing';
+import { ref } from 'vue';
+const publishingStore = usePublishingStore();
 
-<template></template>
+const province = ref('');
+const city = ref('');
+const road = ref('');
 
-<style scoped></style>
+const addAddress = () => {
+  if(province.value && city.value && road.value){
+    publishingStore.form.addressList.push({
+      province: province.value,
+      city: city.value,
+      road: road.value
+    });
+    province.value = '';
+    city.value = '';
+    road.value = '';
+  }
+};
+</script>
+
+<template>
+  <div>
+    <input v-model="province" placeholder="시/도">
+    <input v-model="city" placeholder="시/군/구">
+    <input v-model="road" placeholder="도로명">
+    <button @click="addAddress">주소 등록</button>
+    <button on-click="/HotelRegister">취소</button>
+  </div>
+</template>
