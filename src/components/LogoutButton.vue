a3ccf447477588576a92c7c7f9efcd4c5e8d9737
devaspirant0510 <seungho020510@gmail.com>
2025-09-16 09:18:18 +0900

    feat: 로그아웃 버튼 컴포넌트 추가

---
+++ src/components/LogoutButton.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import axios from 'axios';
+import { apiClient } from '@/utils/axiosClient.ts';
+
+export default defineComponent({
+  name: 'LogoutButton',
+  emits: ['logged-out'],
+  setup(_, { emit }) {
+    const handleLogout = async () => {
+      try {
+        await apiClient.post('../logout');
+        emit('logged-out');
+      } catch (err) {
+        console.error('로그아웃 실패:', err);
+      }
+    };
+    return {
+      handleLogout
+    };
+  }
+});
+</script>
+
+<template>
+  <div @click="handleLogout">
+    <slot>
+      <button>Logout</button>
+    </slot>
+  </div>
+</template>
+
+<style scoped>
+</style>
