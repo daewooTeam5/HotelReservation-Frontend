8560606ebade007f566fc14abb5fb1354262f271
devaspirant0510 <seungho020510@gmail.com>
2025-09-30 14:49:01 +0900

    design 프로필 화면 배치 수정

--- src/layout/OwnerHeader.vue
+++ src/layout/OwnerHeader.vue
@@ -2,6 +2,7 @@
   <header
     class="h-16 bg-white/80 backdrop-blur-md shadow-sm border-b border-gray-200/50 flex items-center justify-between px-6 relative"
   >
+    <div></div>
 
     <h1
       class="absolute left-1/2 transform -translate-x-1/2 text-xl font-bold
