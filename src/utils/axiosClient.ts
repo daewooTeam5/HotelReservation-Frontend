2ea82a539930f6e3243c21705b8c91ad0371243d
devaspirant0510 <seungho020510@gmail.com>
2025-11-13 13:12:54 +0900

    fix: baseurl 변경

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -6,7 +6,7 @@ console.log(import.meta.env.VITE_SERVER_URL);
 
 // Axios 인스턴스 생성
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URL,
+  baseURL: import.meta.env.VITE_SERVER_URL+"/api",
   // 기본 Content-Type 설정은 여기서 삭제합니다.
   withCredentials: true,
 });
